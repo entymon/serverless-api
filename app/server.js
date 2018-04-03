ced65e24b2911d4a85020ae3c6794a8c79d60310
entymon <pawel.olejniczak.i@gmail.com>
2018-04-03 17:03:46 +0100

    Added endpoint with param in url

--- app/server.js
+++ app/server.js
@@ -6,6 +6,12 @@ app.get('/', function (req, res) {
   res.send('Hello World home!')
 });
 
+app.get('/norman/:id', function (req, res) {
+  res.json({
+    id: req.params.id
+  });
+});
+
 app.get('/express', function (req, res) {
   res.send('Hello World help!')
 });
