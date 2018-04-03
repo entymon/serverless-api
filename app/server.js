9ecac7a357a2e931b956257337180734e27688a0
entymon <pawel.olejniczak.i@gmail.com>
2018-04-03 18:59:14 +0100

    Syntax in yml file

--- app/server.js
+++ app/server.js
@@ -1,7 +1,24 @@
 const express = require('serverless-express/express');
+const bodyParser = require('body-parser');
+const AWS = require('aws-sdk');
 
 const app = express();
+const USERS_TABLE = process.env.USERS_TABLE;
 require('dotenv').config();
+
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDb;
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  });
+  console.log(dynamoDb);
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+}
+
+app.use(bodyParser.json({ strict: false }));
 
 app.get('/', function (req, res) {
   res.send('Hello World home!');
@@ -19,6 +36,56 @@ app.get('/express', function (req, res) {
 
 app.get('/test', function (req, res) {
   res.send('Hello World test!');
+});
+
+
+// Get User endpoint
+app.get('/users/:userId', function (req, res) {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  };
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not get user' });
+    }
+    if (result.Item) {
+      const { userId, name } = result.Item;
+      res.json({ userId, name });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  });
+});
+
+// Create User endpoints
+app.post('/users', function (req, res) {
+  const { userId, name } = req.body;
+  if (typeof userId !== 'string') {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } else if (typeof name !== 'string') {
+    res.status(400).json({ error: '"name" must be a string' });
+  }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId: userId,
+      name: name,
+    },
+  };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not create user' });
+    }
+    res.json({ userId, name });
+  });
 });
 
 module.exports = app;
