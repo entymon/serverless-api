049ff9a913ac18f1c14140443ec3a3b80aba0b9e
entymon <pawel.olejniczak.i@gmail.com>
2018-05-10 18:31:09 +0100

    Tests with swagger and node

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { validateBody, presence, isArray } = require('property-validator');
 const router = express.Router();
 const {
   createPost,
@@ -16,22 +17,65 @@ router.get('/:uuid', (req, res, next) => {
   getPostByUuid(req.params.uuid).then(data => res.json(data));
 });
 
+/**
+ *
+ *  const post = {
+      title: [string],
+      content: [string],
+      author: {
+        uuid: 'sdassadasdwewe',
+        name: 'Pawel',
+      },
+      categories: [
+        'asdwewew', 'weweweerewr'
+      ]
+    };
+ */
+
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *    description: Create new post in DynamoDB
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - name: username
+ *        description: Username to use for login.
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      - name: password
+ *        description: User's password.
+ *        in: formData
+ *        required: true
+ *        type: string
+ *    responses:
+ *      '201':
+ *        description: Information about created object
+ */
 router.post('/', (req, res, next) => {
 
-  console.log(req.body);
-  // TODO: Add validation for each field etc
-  const post = {
-    title: 'test title my new post',
-    content: 'lorem ipsum dolor semit and etc trlala cola cran',
-    author: {
-      uuid: 'sdassadasdwewe',
-      name: 'Pawel',
-    },
-    categories: [
-      'asdwewew', 'weweweerewr'
-    ]
-  };
-  createPost(post).then(data => res.json(data));
+  const validation = validateBody(req, [
+    presence('title'),
+    presence('content'),
+    presence('author.uuid'),
+    presence('author.username'),
+    presence('categories'),
+    isArray('categories')
+  ]);
+
+  if (validation.valid) {
+    res.status(200).send('Welcome!');
+    createPost(req.body)
+      .then(data => res.json({
+        status: 200,
+        message: 'post was added',
+        body: data
+      }));
+  } else {
+    res.status(422).send({ errors: validation.errors });
+  }
 });
 
 router.put('/:uuid', (req, res, next) => {
--- app/server.js
+++ app/server.js
@@ -4,9 +4,30 @@ const express = require('serverless-express/express');
 const bodyParser = require('body-parser');
 const routes = require('./controllers/index');
 
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: 'Serverless API CMS',
+      version: '1.0.0',
+    },
+  },
+  apis: [routes],
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+
 const app = express();
 
 app.use(bodyParser.json({ strict: false }));
+
+app.get('/api-docs.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
 app.use('/', routes);
 
 module.exports = app;
